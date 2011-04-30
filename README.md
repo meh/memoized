515fcedd43316288b46574fbb814641ba76ac8ec
meh <meh@paranoici.org>
2011-04-30 12:39:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+memoized
+========
+
+I wasn't completely happy with the actual memoization gems so I made one myself.
+
+```ruby
+class Perl
+  memoize
+  def version
+    `perl -MConfig -e 'print $Config{version};'`
+  end
+end
+
+p = Perl.new
+p.version # this will execute the method and cache the result
+# subsequent calls will get the result from the cache
+
+p.memoize_clear(:version) # this will clear the cache for version only
+p.memoize_clear # this will clear the cache for every memoized method
+# the caching is instance based, not class based, if you want class based
+# caching just make a singleton :)
+
+p.memoized_cache # this will return the cache (which is a simple Hash)
+```
---
+++ lib/memoized.rb
@@ -0,0 +1,51 @@
+#--
+#          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                  Version 2, December 2004
+#
+# Copyleft meh.
+#
+#          DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+#++
+
+class Class
+  def method_added (name)
+    if @__to_memoize__
+      memoize(name)
+    end
+  end
+end
+
+class Object
+  # Memoize the method +name+. If +file+ is provided, then the method results
+  # are stored on disk as well as in memory.
+  def memoize (name=nil)
+    if !name
+      return @__to_memoize__ = true
+    end
+    
+    @__to_memoize__ = false
+
+    name = name.to_sym
+    meth = self.instance_method(name)
+
+    define_method name do |*args|
+      memoized_cache[[name] + [args]] ||= meth.bind(self).call(*args)
+    end
+  end
+
+  # Clear the memoized cache completely or only for the method +name+
+  def memoize_clear (name=nil, file=nil)
+    if name
+      memoized_cache.delete(name.to_sym)
+    else
+      memoized_cache.clear
+    end
+  end
+
+  def memoized_cache
+    @__memoized_cache__ ||= {}
+  end
+end
