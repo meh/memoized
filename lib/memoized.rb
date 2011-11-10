288fbaf15ac99fa60c9ed4481d2d045cf6cd47ad
meh <meh@paranoici.org>
2011-11-10 03:39:55 +0100

    Fixes for #singleton_memoize

--- lib/memoized.rb
+++ lib/memoized.rb
@@ -20,7 +20,7 @@ class Module
 	end
 
 	refine_method :singleton_method_added do |old, name|
-		singleton_memoized(name) if defined?(@@__to_memoize__) && @@__to_memoize__
+		singleton_memoized(name) if @__to_singleton_memoize__
 
 		old.call(name)
 	end
@@ -28,7 +28,7 @@ end
 
 class Object
 	# Memoize the method +name+.
-	def memoized (name=nil)
+	def memoized (name = nil)
 		return if @__to_memoize__ = !name
 
 		refine_method name do |old, *args|
@@ -43,8 +43,8 @@ class Object
 	end; alias memoize memoized
 
 	# Memoize the singleton method +name+.
-	def singleton_memoized (name=nil)
-		return if name.nil? || defined?(@@__to_memoize__) && @@__to_memoize__ = !name
+	def singleton_memoized (name = nil)
+		return if @__to_singleton_memoize__ = !name
 
 		refine_singleton_method name do |old, *args|
 			if memoized_cache[name].has_key?(args)
@@ -58,7 +58,7 @@ class Object
 	end; alias singleton_memoize singleton_memoized
 
 	# Clear the memoized cache completely or only for the method +name+
-	def memoized_clear (name=nil)
+	def memoized_clear (name = nil)
 		if name
 			memoized_cache.delete(name.to_sym)
 		else
@@ -68,17 +68,16 @@ class Object
 
 	# Get the memoization cache
 	def memoized_cache
-		@__memoized_cache__ ||= Hash.new {|hash, key|
-			hash[key] = {}
-		}
+		@__memoized_cache__ ||= Hash.new { |h, k| h[k] = {} }
 	end; alias memoize_cache memoized_cache
 
 	private
-		def __memoized_try_to_clone__ (value) # :nodoc:
-			begin
-				Marshal.load(Marshal.dump(value))
-			rescue Exception
-				value
-			end
+
+	def __memoized_try_to_clone__ (value) # :nodoc:
+		begin
+			Marshal.load(Marshal.dump(value))
+		rescue Exception
+			value
 		end
+	end
 end
